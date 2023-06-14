095e02b7d387934a32e2ed8b9246ca66881577a0
jack-thant <thanthtoo5728@gmail.com>
2023-06-14 13:10:27 +0800

    Implement Redis db with send and pull functions

--- rpc-server/handler.go
+++ rpc-server/handler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"math/rand"
+	"time"
 
 	"github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc"
 )
@@ -11,14 +12,71 @@ import (
 type IMServiceImpl struct{}
 
 func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.SendResponse, error) {
+	if err := validateSendRequest(req); err != nil {
+		return nil, err
+	}
+
+	timestamp := time.Now().Unix()
+	message := &Message{
+		Message:   req.Message.GetText(),
+		Sender:    req.Message.GetSender(),
+		Timestamp: float64(timestamp),
+	}
+
+	roomID, _ := getRoomID(req.Message.GetChat())
+
+	err := rdb.SaveMessage(ctx, roomID, message)
+	if err != nil {
+		return nil, err
+	}
+
 	resp := rpc.NewSendResponse()
-	resp.Code, resp.Msg = areYouLucky()
+	resp.Code, resp.Msg = 0, "success"
 	return resp, nil
 }
 
 func (s *IMServiceImpl) Pull(ctx context.Context, req *rpc.PullRequest) (*rpc.PullResponse, error) {
+	roomID, err := getRoomID(req.GetChat())
+	if err != nil {
+		return nil, err
+	}
+
+	start := req.GetCursor()
+	end := start + int64(req.GetLimit()) // did not minus 1 on purpose for hasMore check later on
+
+	messages, err := rdb.GetMessagesByRoomID(ctx, roomID, start, end, req.GetReverse())
+	if err != nil {
+		return nil, err
+	}
+
+	respMessages := make([]*rpc.Message, 0)
+	var counter int32 = 0
+	var nextCursor int64 = 0
+	hasMore := false
+	for _, msg := range messages {
+		if counter+1 > req.GetLimit() {
+			// having extra value here means it has more data
+			hasMore = true
+			nextCursor = end
+			break // do not return the last message
+		}
+		temp := &rpc.Message{
+			Chat:     req.GetChat(),
+			Text:     msg.Message,
+			Sender:   msg.Sender,
+			SendTime: int64(msg.Timestamp),
+		}
+		respMessages = append(respMessages, temp)
+		counter += 1
+	}
+
 	resp := rpc.NewPullResponse()
-	resp.Code, resp.Msg = areYouLucky()
+	resp.Messages = respMessages
+	resp.Code = 0
+	resp.Msg = "success"
+	resp.HasMore = &hasMore
+	resp.NextCursor = &nextCursor
+
 	return resp, nil
 }
 
